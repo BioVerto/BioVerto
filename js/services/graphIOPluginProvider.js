20692ef4607902f860e9660f82b6a8e9334b9ec3
KartikGc <chikarthik@gmail.com>
2014-02-01 11:12:05 -0500

    added several services

--- js/services/graphIOPluginProvider.js
+++ js/services/graphIOPluginProvider.js
@@ -1,28 +1,7 @@
 angular.module("MyApp")
     .service('graphIOPluginProvider', function(graphProvider,d3){
-var csvPlugin = function(g5,blob, source, target){
-            source = source || "source";
-            target = target || "target";
-            var graph = graphProvider.createGraph();
-            var data = d3.csv.parse(blob);
-                data.forEach(function(d){
-                   // assuming that source and target columns are defined
-                  
-                    var s = d[source];
-                    delete d[source];
-                    var t = d[target];
-                    delete d[target];
-                    graph.addEdge(s, t, d);
-                  
-                 });
-                // select the first element and add accessor functions for mebers
-                var el = data[0] || {};
-                
-                for (v in el){
-                    g5.addEdgeAccessor(v, g5.createAccessor(v));
-                }
-
-            return graph;
+var csvPlugin = function(g5,blob){
+            return d3.csv.parse(blob);
             }
  this.getPlugin = function (plugin){
                 switch (plugin)
---
+++ js/services/modalCtrlProvider.js
@@ -0,0 +1,51 @@
+angular.module("MyApp")
+        .service('modalCtrlProvider', function(graphExecutionEngine, fileLoaderService) {
+        var csvCtrl = function($scope, $modalInstance) {
+               
+        $scope.state = 'uploadState';
+                $scope.handleFileSelect = function(element) {
+                $scope.file = element.files[0]; // FileList object
+                }
+        $scope.ok = function(graphName) {
+        graphExecutionEngine.loadGraphFromObjArray($scope.objArr,graphName,$scope.source,$scope.target);    
+        $modalInstance.close();
+    }
+    var myHeaderCellTemplate = '<div class="ngHeaderSortColumn {{col.headerClass}}"><div>{{col.displayName}} </div>'+
+                         '<input type="radio" ng-model=\"$parent.$parent.$parent.$parent.source\" value="{{col.displayName}}">  Source <br/>'+
+                         '<input type="radio" ng-model=\"$parent.$parent.$parent.$parent.target\" value="{{col.displayName}}"> Target <br/></div>';
+                 
+            $scope.debug = function(text)
+            {
+                console.log($scope.source);
+            }
+            $scope.fileUpload = function()
+                {
+                fileLoaderService.loadFile($scope.file, function(e){
+                $scope.state = 'previewState';
+                        $scope.blob = e.target.result;
+                        $scope.objArr = graphExecutionEngine.generateObjArray("csv", e.target.result);
+                         $scope.columns =[];
+                        
+                        for(key in $scope.objArr[0] )
+                        {
+                           var tempObj = {field:key.toString(),displayName:key.toString(),headerCellTemplate :myHeaderCellTemplate}; 
+                           $scope.columns.push(tempObj);
+                          
+                    }
+                });
+                };
+       
+                $scope.gridOptions = { enableColumnResize:true,headerRowHeight:70,enablePinning: true, data:'objArr',  columnDefs: 'columns',virtualizationThreshold:10, enableSorting:false,b:'a'};
+                        $scope.cancel = function() {
+                        $modalInstance.dismiss('cancel');
+                        };
+                };
+                this.getCtrl = function(type)
+                {
+                switch (type)
+                {
+                case 'csv':
+                        return csvCtrl;
+                }
+                }
+        });
