2928d02171e74f2d1f3a47963ecd1c62ecb659f4
KartikGc <chikarthik@gmail.com>
2014-02-01 11:27:03 -0500

    Fix for the upload button

--- js/services/modalCtrlProvider.js
+++ js/services/modalCtrlProvider.js
@@ -1,51 +1,52 @@
 angular.module("MyApp")
         .service('modalCtrlProvider', function(graphExecutionEngine, fileLoaderService) {
-        var csvCtrl = function($scope, $modalInstance) {
-               
-        $scope.state = 'uploadState';
+            var csvCtrl = function($scope, $modalInstance) {
+                $scope.uploadButtonEnable = true;
+                $scope.state = 'uploadState';
                 $scope.handleFileSelect = function(element) {
-                $scope.file = element.files[0]; // FileList object
+                    $scope.file = element.files[0]; // FileList object
                 }
-        $scope.ok = function(graphName) {
-        graphExecutionEngine.loadGraphFromObjArray($scope.objArr,graphName,$scope.source,$scope.target);    
-        $modalInstance.close();
-    }
-    var myHeaderCellTemplate = '<div class="ngHeaderSortColumn {{col.headerClass}}"><div>{{col.displayName}} </div>'+
-                         '<input type="radio" ng-model=\"$parent.$parent.$parent.$parent.source\" value="{{col.displayName}}">  Source <br/>'+
-                         '<input type="radio" ng-model=\"$parent.$parent.$parent.$parent.target\" value="{{col.displayName}}"> Target <br/></div>';
-                 
-            $scope.debug = function(text)
-            {
-                console.log($scope.source);
-            }
-            $scope.fileUpload = function()
+                $scope.ok = function(graphName) {
+                    graphExecutionEngine.loadGraphFromObjArray($scope.objArr, graphName, $scope.source, $scope.target);
+                    $modalInstance.close();
+                }
+                var myHeaderCellTemplate = '<div class="ngHeaderSortColumn {{col.headerClass}}"><div>{{col.displayName}} </div>' +
+                        '<input type="radio" ng-model=\"$parent.$parent.$parent.$parent.source\" value="{{col.displayName}}">  Source <br/>' +
+                        '<input type="radio" ng-model=\"$parent.$parent.$parent.$parent.target\" value="{{col.displayName}}"> Target <br/></div>';
+
+                $scope.fileUpload = function()
                 {
-                fileLoaderService.loadFile($scope.file, function(e){
-                $scope.state = 'previewState';
+                    $scope.uploadButtonEnable = false;
+                    console.log("a");
+                    fileLoaderService.loadFile($scope.file, function(e) {
                         $scope.blob = e.target.result;
                         $scope.objArr = graphExecutionEngine.generateObjArray("csv", e.target.result);
-                         $scope.columns =[];
-                        
-                        for(key in $scope.objArr[0] )
+                        $scope.columns = [];
+                        for (key in $scope.objArr[0])
                         {
-                           var tempObj = {field:key.toString(),displayName:key.toString(),headerCellTemplate :myHeaderCellTemplate}; 
-                           $scope.columns.push(tempObj);
-                          
-                    }
-                });
+                            var tempObj = {field: key.toString(), displayName: key.toString(), headerCellTemplate: myHeaderCellTemplate};
+                            $scope.columns.push(tempObj);
+
+                        }
+                        $scope.state = 'previewState';
+                        $scope.$apply();
+                        $scope.uploadButtonEnable = true;  
+                  
+                    });
+                    
                 };
-       
-                $scope.gridOptions = { enableColumnResize:true,headerRowHeight:70,enablePinning: true, data:'objArr',  columnDefs: 'columns',virtualizationThreshold:10, enableSorting:false,b:'a'};
-                        $scope.cancel = function() {
-                        $modalInstance.dismiss('cancel');
-                        };
+
+                $scope.gridOptions = {enableColumnResize: true, headerRowHeight: 70, enablePinning: true, data: 'objArr', columnDefs: 'columns', virtualizationThreshold: 10, enableSorting: false, b: 'a'};
+                $scope.cancel = function() {
+                    $modalInstance.dismiss('cancel');
                 };
-                this.getCtrl = function(type)
-                {
+            };
+            this.getCtrl = function(type)
+            {
                 switch (type)
                 {
-                case 'csv':
+                    case 'csv':
                         return csvCtrl;
                 }
-                }
+            }
         });
