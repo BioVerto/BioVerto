31a70518f45179e0118915f92ddb4b77c394f6bf
KartikGc <chikarthik@gmail.com>
2014-02-13 11:26:45 -0500

    degree fix

--- js/graphLib/g5-base-algos.js
+++ js/graphLib/g5-base-algos.js
@@ -3,34 +3,60 @@
 /* This file contains basic algorithmic pluggins */
 
 
-// Out degree computation. Simply counts the numer of out degrees
+//// Out degree computation. Simply counts the numer of out degrees
+//(function(g5) {
+//    // new field and register accessor for it under algo name
+//    var f1 = g5.newField();
+//    var f2 = g5.newField();
+//
+//    g5.addAlgoPlugin({
+//        name: "Degree Centrality",
+//        algo: function(g) {
+//            for (var i in g.nodes) {
+//           g.nodes[i].data[f2]=0;
+//            }
+//            for (var i in g.nodes) {
+//                var node = g.nodes[i];
+//                node.data[f1] = node.edges.length;
+//                for (j in node.edges)
+//                {
+//                        innode = node.edges[j].target.data[f2]++;
+// 
+//                }
+//            }
+//        },
+//        nodeAccs: {
+//            "Out Degree": {type: "number", fct: g5.createAccessor(f1)},
+//            "In Degree": {type: "number", fct: g5.createAccessor(f2)},
+//        }});
+//}(g5));
 (function(g5) {
     // new field and register accessor for it under algo name
-    var f1 = g5.newField();
-    var f2 = g5.newField();
-
+    var f = g5.newField();
     g5.addAlgoPlugin({
         name: "Degree Centrality",
         algo: function(g) {
             for (var i in g.nodes) {
-           g.nodes[i].data[f2]=0;
+                g.nodes[i].data[f] = 0;
             }
-            for (var i in g.nodes) {
-                var node = g.nodes[i];
-                node.data[f1] = node.edges.length;
-                for (j in node.edges)
+            for (var i in g.edges) {
+                for (var j = i - 1; j > -1; j--) {
+                    if (g.edges[i].source === g.edges[j].target && g.edges[i].target === g.edges[j].source)
+                    {
+                        break;
+                    }
+                }
+                if(j===-1)
                 {
-                        innode = node.edges[j].target.data[f2]++;
- 
+                    g.edges[i].source.data[f]++;
+                    g.edges[i].target.data[f] ++;
                 }
             }
         },
         nodeAccs: {
-            "Out Degree": {type: "number", fct: g5.createAccessor(f1)},
-            "In Degree": {type: "number", fct: g5.createAccessor(f2)},
+            "Degree": {type: "number", fct: g5.createAccessor(f)},
         }});
 }(g5));
-
 
 // Random data node algo
 (function(g5) {
